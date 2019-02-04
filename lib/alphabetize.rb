7efdb3b911c2d9ffeede150fbad89a3da7a2f831
H B <phajib@gmail.com>
2019-02-04 10:43:59 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |str|
+    str.split('').collect do |letter|
+      esperanto_alphabet.index(letter)
+    end
+  end
 end
